87a897a202f290ab2438bf90b7749d6000ec4822
Cindy Michalowski <chiwhitesox56@gmail.com>
2021-05-24 07:43:31 -0400

    Pushing to move off of work computer

--- src/services/date.js
+++ src/services/date.js
@@ -1,7 +1,10 @@
 import { DateTime } from "luxon";
 
-export const getTodaysDate = () => DateTime.now().toLocaleString({month: 'long', day: 'numeric'});
-  
-export const getCurrentWeekday = () => DateTime.now().toLocaleString({weekday: 'long'});
+export const getTodaysDate = () =>
+  DateTime.now().toLocaleString({ month: "long", day: "numeric" });
 
-export const getForecastWeekday = (dt) => DateTime.fromSeconds(dt).toFormat("cccc");
+export const getCurrentWeekday = () =>
+  DateTime.now().toLocaleString({ weekday: "long" });
+
+export const getForecastWeekday = (dt) =>
+  DateTime.fromSeconds(dt).toFormat("cccc");
--- src/services/weather.js
+++ src/services/weather.js
@@ -1,8 +1,8 @@
 export const kelvinToFarenheit = (tempKelvin) => {
-    return Math.round(((tempKelvin - 273.15) * 9/5) + 32) + "°F";
-}
+  return Math.round(((tempKelvin - 273.15) * 9) / 5 + 32) + "°F";
+};
 
 export const unixToStandardTime = (unixTime) => {
-    var dateObj = new Date(unixTime * 1000);
-    return dateObj.toLocaleTimeString()
-}
+  var dateObj = new Date(unixTime * 1000);
+  return dateObj.toLocaleTimeString();
+};
